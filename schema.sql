3050012757c6d93b52407678f309041e130c3b8a
MistaWizard <tylermckellar@gmail.com>
2019-01-22 20:33:08 -0500

    Added SQL Schema

---
+++ schema.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS top_songsDB;
+
+CREATE DATABASE top_songsdb;
+
+USE top_songsdb;
+
+CREATE TABLE Top5000(
+  position INT NOT NULL,
+  artist VARCHAR(150) NOT NULL,
+  song VARCHAR(150) NOT NULL,
+  year INT NULL,
+  raw_total DECIMAL(10,4) NULL,
+  raw_usa DECIMAL(10,4) NULL,
+  raw_uk DECIMAL(10,4) NULL,
+  raw_eur DECIMAL(10,4) NULL,
+  raw_row DECIMAL(10,4) NULL,
+  PRIMARY KEY (position)
+  );
+  
+  select * from top5000;
